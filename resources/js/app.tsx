5a66725ba55fcfe1f58589c1d48047ce5c72212d
vladislav <vlad60870@gmail.com>
2020-06-27 13:46:02 +0300

    Start Project

---
+++ resources/js/app.tsx
@@ -0,0 +1,15 @@
+import bootstrap from './bootstrap.js'
+import Input from './components/Input'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+export default function App() {
+    return (
+        <div>
+            <Input>
+
+            </Input>
+        </div>
+    )
+}
